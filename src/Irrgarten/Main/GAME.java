5d9c8551fe080b255d4b51c9f3a8323d715eca4c
aaron <aaron@aaron-Victus-by-HP-Gaming-Laptop-15-fa0xxx>
2025-11-07 11:36:19 +0100

    nuevo main

---
+++ src/Irrgarten/Main/GAME.java
@@ -0,0 +1,29 @@
+package Irrgarten.Main;
+
+/**
+ *
+ * @author aaron
+ */
+import Irrgarten.Game;
+import Irrgarten.controller.Controller;
+import Irrgarten.UI.TextUI;
+
+public class GAME {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("\n--- JUEGO IRRGARTEN ---\n");
+
+        int nPlayers = 2; 
+
+        Game game = new Game(nPlayers);
+
+        TextUI view = new TextUI();
+
+        Controller controller = new Controller(game, view);
+        controller.play();
+        
+        // Fin del juego
+        System.out.println("\n*** ¡FIN DEL JUEGO! ***");
+    }
+}
