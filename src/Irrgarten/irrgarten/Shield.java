83ae06b7132391b11a1121ed01bf434e05fb20bf
aaron <aaron@aaron-Victus-by-HP-Gaming-Laptop-15-fa0xxx>
2025-11-09 16:57:14 +0100

    update

---
+++ src/Irrgarten/irrgarten/Shield.java
@@ -0,0 +1,43 @@
+package Irrgarten.irrgarten;
+
+/**
+ *
+ * @author aaron
+ */
+public class Shield {
+   private final float protection;
+   private int uses;
+   
+   public Shield(float _protection, int _uses){
+       protection = _protection;
+       uses = _uses;
+   }
+
+    public int getUses(){
+        return uses;
+    }
+    
+    public float getProtection(){
+        return protection;
+    }
+   
+    public float protect(){
+        float result = 0;
+        
+        if(uses > 0){
+            result = protection;
+            --uses;
+        }
+        
+        return result;
+    }
+    
+    @Override 
+    public String toString(){
+        return ("S[" + protection + ", " + uses + "]");
+    }
+    
+    public boolean discard(){
+        return Dice.discardElement(uses);
+    }
+}
