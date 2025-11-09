6ceeb0160a9c25e47f0af258d9e43972c96678d1
aaron <aaron@aaron-Victus-by-HP-Gaming-Laptop-15-fa0xxx>
2025-11-09 16:56:46 +0100

    update

---
+++ src/Irrgarten/irrgarten/Monster.java
@@ -0,0 +1,61 @@
+package Irrgarten.irrgarten;
+
+/**
+ *
+ * @author aaron
+ */
+public class Monster {
+    private final static int INITIAL_HEALTH = 5;
+    
+    private final String name;
+    private final float intelligence;
+    private final float strength;
+    private float health;
+    private int row;
+    private int col;
+    
+    public Monster(String _name, float _intelligence, float _strength){
+        name = _name;
+        intelligence = _intelligence;
+        strength = _strength;
+        health = INITIAL_HEALTH;
+    }
+    
+    public boolean dead(){
+        return !(health > 0);
+    }
+    
+    public float attack(){
+        return Dice.intensity(strength);
+    }
+    
+    public void setPos(int _row, int _col){
+        row = _row;
+        col = _col;
+    }    
+    
+    @Override
+    public String toString(){
+        return "\n\nNombre: " + name + "\nPosicion: (" + row + "," + col + ")\nIntelligence: " + intelligence + 
+                "\nStrength: " + strength + "\nHealth: " + health + "\n";
+    }
+    
+    public void gotWounded(){
+        --health;
+    }
+    
+    
+    public boolean defend(float receivedAttack){ // tiene que ser publico para poder usarlo en otras clases
+        boolean isDead = dead();
+        if(!isDead){
+            float defensiveEnergy = Dice.intensity(intelligence);
+            
+            if(defensiveEnergy < receivedAttack){
+                gotWounded();
+                isDead = dead();
+            }
+        }
+        
+        return isDead;
+    }
+}
