7d209c21a085a89d8c7dd65d9b7a78dd87732567
aaron <aaron@aaronpc.tailac586f.ts.net.>
2025-11-25 18:26:42 +0100

    actualizado

--- src/Irrgarten/Orientation.java
+++ src/Irrgarten/Orientation.java
@@ -1,10 +1,5 @@
 package Irrgarten;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
-
 /**
  *
  * @author aaron
