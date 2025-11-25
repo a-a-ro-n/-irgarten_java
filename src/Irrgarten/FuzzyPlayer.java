617b2db913149c2f1e101579ac47754a6c46f085
aaron <aaron@aaronpc.tailac586f.ts.net.>
2025-11-25 18:10:31 +0100

    actualizado los metodos

--- src/Irrgarten/FuzzyPlayer.java
+++ src/Irrgarten/FuzzyPlayer.java
@@ -9,11 +9,26 @@ import java.util.ArrayList;
 public class FuzzyPlayer extends Player{
     
     public FuzzyPlayer(Player other){
-        super(other);
+        super(other, other.getNumber());
+        shields = other.shields;
+        weapons = other.weapons;
     }
     
     @Override
     public Directions move(Directions direction, ArrayList<Directions> validMoves){
         return direction;
+    }
+    
+    public float attack(){
+        
+    }
+    
+    abstract float defensiveEnergy(){
+        
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString();
     }
 }
