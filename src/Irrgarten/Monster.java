2b302bf7e572821f92f3b93f4555024873c620b9
aaron <aaron@aaronpc.tailac586f.ts.net.>
2025-11-25 18:10:12 +0100

    actualizado con abstraccion

--- src/Irrgarten/Monster.java
+++ src/Irrgarten/Monster.java
@@ -4,58 +4,40 @@ package Irrgarten;
  *
  * @author aaron
  */
-public class Monster {
+public class Monster extends LabyrinthCharacter{
     private final static int INITIAL_HEALTH = 5;
     
-    private final String name;
-    private final float intelligence;
-    private final float strength;
-    private float health;
-    private int row;
-    private int col;
-    
-    public Monster(String _name, float _intelligence, float _strength){
-        name = _name;
-        intelligence = _intelligence;
-        strength = _strength;
-        health = INITIAL_HEALTH;
+    public Monster(String _name, float _intelligence, float _strength, float _health){
+        super(_name,_intelligence,_strength, INITIAL_HEALTH);
     }
     
+    @Override
     public boolean dead(){
-        return !(health > 0);
+        return super.dead();
     }
     
+    @Override
     public float attack(){
-        return Dice.intensity(strength);
+        return super.attack();
     }
     
+    @Override
     public void setPos(int _row, int _col){
-        row = _row;
-        col = _col;
+        super.setPos(_row, _col);
     }    
     
     @Override
     public String toString(){
-        return "\n\nNombre: " + name + "\nPosicion: (" + row + "," + col + ")\nIntelligence: " + intelligence + 
-                "\nStrength: " + strength + "\nHealth: " + health + "\n";
+        return super.toString();
     }
     
     public void gotWounded(){
-        --health;
+        // --health;
     }
     
     
+    @Override
     public boolean defend(float receivedAttack){ // tiene que ser publico para poder usarlo en otras clases
-        boolean isDead = dead();
-        if(!isDead){
-            float defensiveEnergy = Dice.intensity(intelligence);
-            
-            if(defensiveEnergy < receivedAttack){
-                gotWounded();
-                isDead = dead();
-            }
-        }
-        
-        return isDead;
+        return super.defend(receivedAttack);
     }
 }
